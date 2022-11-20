ac96404d97681975fe62e429f3c29940bfb3be28
Oleg <loshkin.oleg.95@gmail.com>
2022-11-20 11:57:17 +0100

    Switching to glm

--- Src/ScoutCommon.cpp
+++ Src/ScoutCommon.cpp
@@ -1,4 +1,3 @@
 // This file is only here to force IDE's to compile a static library for header-only modules.
-#include <Scout/Color.h>
 #include <Scout/PreprocessorMacro.h>
 #include <Scout/TypedefAndEnum.h>
